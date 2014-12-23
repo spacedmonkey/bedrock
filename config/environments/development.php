38a484fddd4b1057c779c74c48e1ef53a075d763
Jonny Harris <spacedmonkey@users.noreply.github.com>
2014-12-23 01:42:36 +0000

    Update development.php

--- config/environments/development.php
+++ config/environments/development.php
@@ -7,6 +7,7 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
+define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
 
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
