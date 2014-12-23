fb6b720c010bbc390709c57277d3ddbc88490005
Jonny Harris <spacedmonkey@users.noreply.github.com>
2014-12-23 01:42:23 +0000

    Update production.php

--- config/environments/production.php
+++ config/environments/production.php
@@ -7,6 +7,8 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
+define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
+
 
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
