ec02054c17a031d1ccacc942e252958fa89a0dfa
spacedmonkey <spacedmonkey2@gmail.com>
2015-01-05 23:09:54 +0000

    Changed config files

--- config/application.php
+++ config/application.php
@@ -85,7 +85,6 @@ define('DISALLOW_FILE_MODS', true );
 
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
